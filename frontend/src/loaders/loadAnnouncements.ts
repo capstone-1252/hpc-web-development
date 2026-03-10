145dcb9cafdf17cb3bddaa2ec86ff3f39f7a4f77
Curtis <cjododev@gmail.com>
2026-03-10 17:18:27 -0600

    load only most recent(30 days) announcements

--- frontend/src/loaders/loadAnnouncements.ts
+++ frontend/src/loaders/loadAnnouncements.ts
@@ -1,4 +1,4 @@
-import cockpit, { type CockpitItemData } from "@/lib/cockpit"
+import cockpit, { type CockpitItemData, type CockpitOptions } from "@/lib/cockpit"
 
 export interface Announcement extends CockpitItemData {
 	title: string
@@ -11,8 +11,25 @@ const fetchAssociatedEvent = async (id: string) => {
 	return res
 }
 
-export const loadAnnouncements = async () => {
-	const res = await cockpit.getItems<Announcement>("announcements")
+const thirtyDaysAgo = Math.floor(Date.now() / 1000) - (30 * 24 * 60 * 60) // 30 days
+// testing purposes
+// const oneDayAgo = Math.floor(Date.now() / 1000) - 86400;
+
+export const loadLatestAnnouncements = async () => {
+	// cockpit uses Mongo Query syntax: ref https://www.mongodb.com/docs/manual/reference/mql/query-predicates/
+	// to keep connie from having to constantly manage announcements and keep data sent to client small after time, 
+	// only fetch the most recent announcements
+	const query = {
+		_modified: {
+			$gte: thirtyDaysAgo
+		}
+	}
+
+	const options: CockpitOptions = {
+		filter: query
+	}
+
+	const res = await cockpit.getItems<Announcement>("announcements", options)
 	res.map(async (announcement) => {
 		if (announcement.event) {
 			announcement.event = await fetchAssociatedEvent(announcement.event._id)
