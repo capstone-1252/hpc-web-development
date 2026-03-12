1d69e19bf0d101ec986c8778328acab783e16553
Curtis <cjododev@gmail.com>
2026-03-12 16:17:44 -0600

    use grid-cols-3

--- frontend/src/components/FoodBanks/FoodBanksList.tsx
+++ frontend/src/components/FoodBanks/FoodBanksList.tsx
@@ -28,7 +28,7 @@ export function FoodBanksList() {
 	}
 
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+		<div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
 			{foodBanks.map((foodBank) => (
 				<div key={foodBank._id} className="bg-white p-6 rounded-lg shadow-lg border border-gray-200">
 					<div className="flex flex-col items-start gap-4">
