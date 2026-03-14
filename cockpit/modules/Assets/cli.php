f95e2ed3ad2e26d03e332578952062fb38e41fe5
Curtis <cjododev@gmail.com>
2026-03-14 09:01:07 -0600

    remove large cockpit dir

--- cockpit/modules/Assets/cli.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!isset($cli, $app) || PHP_SAPI !== 'cli') {
-    return;
-}
-
-$cli->add(new Assets\Command\FixVisibilityPermissions($app));
-$cli->add(new Assets\Command\Thumbhash($app));
-$cli->add(new Assets\Command\GeneratePresets($app));
