309313eca1b51c28c0e08214c4e84ccf8fbbf47a
Curtis <cjododev@gmail.com>
2026-03-15 20:38:49 -0600

    Add load hours

---
+++ frontend/src/loaders/loadHours.ts
@@ -0,0 +1,12 @@
+import cockpit, { type CockpitItemData } from "@/lib/cockpit"
+
+export interface WeeklyHours extends CockpitItemData {
+	open: string
+	close: string
+	day: string
+}
+
+export const loadWeeklyHours = async (): Promise<WeeklyHours[]> => {
+	const res = await cockpit.getItems<WeeklyHours>("weeklyhours")
+	return res
+}
