889ef641cf073a3fa7b5690640f721c9874f5b0a
Curtis <cjododev@gmail.com>
2026-03-19 11:19:18 -0600

    update BoardList layout

--- frontend/src/components/Board/BoardMemberCard.tsx
+++ frontend/src/components/Board/BoardMemberCard.tsx
@@ -5,14 +5,17 @@ interface BoardMemberCardProps {
   member: BoardMember;
 }
 export const BoardMemberCard = ({ member }: BoardMemberCardProps) => {
-  console.log(getImageUrl(member.photo, 64, 64));
+	let imageUrl = ""
+	if (member.photo) {
+		imageUrl = getImageUrl(member.photo, 64, 64);
+	}
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg border border-gray-200 text-center">
+    <div className="bg-white p-6 rounded-lg shadow-lg border border-gray-200">
       <div className="w-16 h-16 bg-[#e25002] bg-opacity-10 rounded-full flex items-center justify-center mx-auto mb-4 overflow-hidden">
         {member.photo ? (
           <img
-            src={getImageUrl(member.photo, 64, 64)}
+            src={imageUrl}
             alt={"Headshot of" + member.name}
             className="w-full h-full object-cover"
           />
