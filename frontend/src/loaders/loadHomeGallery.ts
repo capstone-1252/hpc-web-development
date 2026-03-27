866109b1415d7598f1269c2c90a984bb80eca71e
Curtis <cjododev@gmail.com>
2026-03-27 14:54:36 -0600

    feat: add home gallery component and loader

---
+++ frontend/src/loaders/loadHomeGallery.ts
@@ -0,0 +1,11 @@
+import cockpit, { type CockpitItemData } from "@/lib/cockpit"
+import type { AssetImage } from "./utils"
+
+export interface HomeGallery extends CockpitItemData {
+	photos: AssetImage[]
+}
+
+export const loadHomeGallery = async (): Promise<HomeGallery> => {
+	const res = await cockpit.getSingleton<HomeGallery>("homeGallery")
+	return res
+}
